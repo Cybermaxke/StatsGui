f2f7bbeb5fd60ae18fa0a261a954178c7ffcc1ad
Seppe Volkaerts <seppevolkaerts@hotmail.com>
2013-09-14 14:43:00 +0200

    Remove unused imports.

--- src/main/java/me/cybermaxke/statsgui/plugin/hook/economy/StatsEconomy.java
+++ src/main/java/me/cybermaxke/statsgui/plugin/hook/economy/StatsEconomy.java
@@ -29,7 +29,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.RegisteredServiceProvider;
 
 import me.cybermaxke.statsgui.api.Stats;
 import me.cybermaxke.statsgui.api.StatsGui;
